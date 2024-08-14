4711044be547659c290716549f797d0caf5b24be
buratr <bur.atr1@gmail.com>
2024-08-14 22:51:41 +0300

    path_03

--- app/api/get-records/route.ts
+++ app/api/get-records/route.ts
@@ -6,9 +6,9 @@ export async function GET() {
   try {
     
     const filePath = path.join(process.cwd(), 'app', '/data', 'bd.json');
-    console.log("fileData_0",filePath) 
+    //console.log("fileData_0",filePath) 
     const fileData = fs.readFileSync(filePath, 'utf-8');
-    console.log("fileData:", fileData)
+    //console.log("fileData:", fileData)
     const data = JSON.parse(fileData);
 
     return NextResponse.json(data, { status: 200 });
--- app/api/update-record/route.ts
+++ app/api/update-record/route.ts
@@ -4,41 +4,42 @@ import path from 'path';
 
 export async function POST(request: NextRequest) {
   try {
-    // const { id, name, lastName, SS, dateOfBirth, income } = await request.json();
+     const { id, name, lastName, SS, dateOfBirth, income } = await request.json();
 
     // // Путь к JSON-файлу
-    // const filePath = path.join(process.cwd(), 'app', 'data', 'bd.json');
-    // const fileData = fs.readFileSync(filePath, 'utf-8');
-    // const data = JSON.parse(fileData);
-    // const currentDate = new Date();
-    // const aplicantDate = currentDate.toISOString().split('T')[0];
-    // const loanAmount = Math.round(((income/100)*10)  / 1000) * 1000 
+    const filePath = path.join(process.cwd(), 'app', 'data', 'bd.json');
+    const fileData = fs.readFileSync(filePath, 'utf-8');
+    const data = JSON.parse(fileData);
+    const currentDate = new Date();
+    const aplicantDate = currentDate.toISOString().split('T')[0];
+    const loanAmount = Math.round(((income/100)*10)  / 1000) * 1000 
     
     
-    // let expiration
-    // currentDate.setFullYear(currentDate.getFullYear() +1);
-    // expiration = currentDate.toISOString().split('T')[0];
-    // // Поиск записи по id
-    // const recordIndex = data.findIndex((record: any) => record.id === id);
+    let expiration
+    currentDate.setFullYear(currentDate.getFullYear() +1);
+    expiration = currentDate.toISOString().split('T')[0];
+    // Поиск записи по id
+    const recordIndex = data.findIndex((record: any) => record.id === id);
 
-    // if (recordIndex === -1) {
-    //   return NextResponse.json({ error: 'Record not found' }, { status: 404 });
-    // }
+    if (recordIndex === -1) {
+      return NextResponse.json({ error: 'Record not found' }, { status: 404 });
+    }
 
-    // // Обновление записи
-    // data[recordIndex] = {
-    //   ...data[recordIndex],
-    //   name,
-    //   lastName,
-    //   SS,
-    //   dateOfBirth,
-    //   income,
-    //   aplicantDate,
-    //   loanAmount,
-    //   status:"Awaiting signature",
-    //   expiration,
-    // };
+    // Обновление записи
+    data[recordIndex] = {
+      ...data[recordIndex],
+      name,
+      lastName,
+      SS,
+      dateOfBirth,
+      income,
+      aplicantDate,
+      loanAmount,
+      status:"Awaiting signature",
+      expiration,
+    };
 
+    console.log(data)
     // // Запись обновленных данных обратно в файл
     // fs.writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf-8');
 
