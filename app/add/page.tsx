6a711f94e785666aa6d763d698ff96e132db7ea0
buratr <bur.atr1@gmail.com>
2024-08-14 21:48:29 +0300

    mod_02

--- app/add/page.tsx
+++ app/add/page.tsx
@@ -1,14 +1,15 @@
 "use client";
 
-import { useSearchParams } from 'next/navigation';
-import { useState, useEffect, ChangeEvent } from 'react';
+import { Suspense, useState, useEffect, ChangeEvent } from 'react';
+import { useSearchParams, useRouter } from 'next/navigation';
 import { Navbar } from '../components/Navbar';
-import { useRouter } from 'next/navigation';
 import Image from 'next/image';
 import preloader from "@/app/assets/svgs/preloader.gif"
 
-export default function Add() {
+function ActualAddPage() {
   const router = useRouter();
+  const searchParams = useSearchParams();
+  const id = searchParams.get('id');
 
   const [records, setRecords] = useState<any[]>([]);
   const [maxDate, setMaxDate] = useState('');
@@ -19,52 +20,25 @@ export default function Add() {
   const [SS, setSS] = useState('');
   const [dateOfBirth, setDateOfBirth] = useState('');
   const [income, setIncome] = useState('');
-  
-  // const [aplicantDate, setAplicantDate] = useState('');
-  // const [loanAmount, setLoanAmount] = useState('');
-  // const [status, setStatus] = useState('');
-  // const [expiration, setExpiration] = useState('');
   const [message, setMessage] = useState('');
   const [currentId, setCurrentId] = useState('');
 
-
-
-  const searchParams = useSearchParams();
-  //const id = searchParams.get('id');
-
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      const id = searchParams.get('id');
-      if (id) {
-        setCurrentId(id);
-        fetchRecordById(id);
-      }
+    if (id) {
+      setCurrentId(id);
+      fetchRecordById(id);
     }
-  }, [searchParams]);
-
-
-
-  // useEffect(() => {
-  //   if (id) {
-  //     setCurrentId(id)
-  //     // Здесь можно загрузить текущие данные для этого ID, если необходимо
-  //     fetchRecordById(id);
-  //   }
-  // }, [id]);
+  }, [id]);
 
   useEffect(() => {
-    // Вычисляем дату 18 лет назад
     const eighteenYearsAgo = new Date();
     eighteenYearsAgo.setFullYear(eighteenYearsAgo.getFullYear() - 18);
-    
-    // Форматируем дату в строку YYYY-MM-DD
     const formattedDate = eighteenYearsAgo.toISOString().split('T')[0];
     setMaxDate(formattedDate);
   }, []);
 
   const handleDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setDateOfBirth(event.target.value);
-    console.log(event.target.value)
   };
 
   const handleNumberChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -75,47 +49,32 @@ export default function Add() {
   };
 
   const fetchRecordById = async (id: string) => {
-    try {
-      const response = await fetch('/api/get-records');
-      const data = await response.json();
-      const record = data.find((item: any) => item.id === id);
-  
-      if (record) {
-        setName(record.name || '');
-        setLastName(record.lastName || '');
-        setSS(record.SS || '');
-        setDateOfBirth(record.dateOfBirth || '');
-        setIncome(record.income || '');
-      }
-    } catch (error) {
-      console.error('Error fetching record:', error);
+    const response = await fetch('/api/get-records');
+    const data = await response.json();
+    const record = data.find((item: any) => item.id === id);
+
+    if (record) {
+      setName(record.name || '');
+      setLastName(record.lastName || '');
+      setSS(record.SS || '');
+      setDateOfBirth(record.dateOfBirth || '');
+      setIncome(record.income || '');
     }
   };
 
-  useEffect(() => {
-    const fetchRecords = async () => {
-      const response = await fetch('/api/get-records');
-      const data = await response.json();
-      setRecords(data);
-    };
-
-    fetchRecords();
-  }, []);
-
-
   const handleUpdateRecord = async () => {
-    if (!currentId) {
+    if (!id) {
       setMessage('No ID provided');
       return;
     }
-    setPageBusy(true)
+    setPageBusy(true);
     const response = await fetch('/api/update-record', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        id:currentId,
+        id,
         name,
         lastName,
         SS,
@@ -129,50 +88,20 @@ export default function Add() {
       router.push('/');
     } else {
       setMessage('Failed to update record');
-      setPageBusy(false)
+      setPageBusy(false);
     }
   };
 
-  // const handleAddRecord = async () => {
-  //   const response = await fetch('/api/add-record', {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify({
-  //       name,
-  //       lastName,
-  //       aplicantDate,
-  //       loanAmount,
-  //       status,
-  //       expiration,
-  //     }),
-  //   });
-
-  //   if (response.ok) {
-  //     // Обновление списка записей после успешного добавления
-  //     const updatedRecords = await (await fetch('/api/get-records')).json();
-  //     setRecords(updatedRecords);
-  //   } else {
-  //     alert('Failed to add record');
-  //   }
-  // };
-
   return (
-    
-     <main>
-     <Navbar/>
-    <section className="w-full bg-white flex flex-col justify-start items-center min-h-screen py-40">
-      {pageBusy && <div className='opacity-45  z-10 absolute left-0 right-0 top-0 bottom-0 flex justify-center items-center'>
-        <Image className='w-full h-full object-cover'
-                    src={preloader}
-                    alt="burger"
-                    />
-        </div>}
-    <div className="container px-4">
-      <div>
-        {/* <h1>Records</h1>
-        <div>Current ID: {currentId}</div> */}
+    <main>
+      <Navbar />
+      <section className="w-full bg-white flex flex-col justify-start items-center min-h-screen py-40">
+        {pageBusy && (
+          <div className='opacity-45  z-10 absolute left-0 right-0 top-0 bottom-0 flex justify-center items-center'>
+            <Image className='w-full h-full object-cover' src={preloader} alt="loading" />
+          </div>
+        )}
+        <div className="container px-4">
         <form
         className='max-w-96 mx-auto flex flex-col gap-3'
         onSubmit={(e) => e.preventDefault()}>
@@ -243,18 +172,16 @@ export default function Add() {
           </div>
           
         </form>
+        </div>
+      </section>
+    </main>
+  );
+}
 
-        {/* <h2>Record List</h2>
-        <ul>
-          {records.map((record, index) => (
-            <li key={index}>
-              {record.aplicantName} - {record.aplicantDate} - {record.loanAmount} - {record.status} - {record.expiration}
-            </li>
-          ))}
-        </ul> */}
-      </div>
-     </div>
-    </section>
- </main>
+export default function Add() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ActualAddPage />
+    </Suspense>
   );
 }
