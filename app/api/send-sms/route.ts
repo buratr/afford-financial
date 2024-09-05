b8cbf6251dc33655167f5b9bcc48b7d76f440900
buratr <bur.atr1@gmail.com>
2024-09-05 13:32:03 +0300

    send_sms_01

--- app/api/send-sms/route.ts
+++ app/api/send-sms/route.ts
@@ -9,11 +9,10 @@ export async function POST(request: NextRequest) {
   const accountSid = process.env.TWILIO_ACCOUNT_SID as string;
   const authToken = process.env.TWILIO_AUTH_TOKEN as string;
   const client = twilio(accountSid, authToken);
-
+//console.log(id," ", currentUrl)
   try {
     const message = await client.messages.create({
-      body: `StudyPoint Boston invites you to apply for educational financing through Afford Financial.
-Use this secure link to apply: ${currentUrl}add?id=${id} `,
+      body: `StudyPoint Boston invites you to apply for educational financing through Afford Financial. Use this secure link to apply: ${currentUrl}add?id=${id} `,
       from: '+18449191857',
       to: '+18777804236',
     });
