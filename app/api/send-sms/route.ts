1c4d1ad801c158aa8c77b426fb324622c3d14c68
buratr <bur.atr1@gmail.com>
2024-09-05 15:03:57 +0300

    send_sms_04

--- app/api/send-sms/route.ts
+++ app/api/send-sms/route.ts
@@ -6,24 +6,19 @@ import twilio from 'twilio';
 export async function POST(request: NextRequest) {
 
   let {id, currentUrl} = await request.json();
-
-
-
    const accountSid = process.env.TWILIO_ACCOUNT_SID as string;
    const authToken = process.env.TWILIO_AUTH_TOKEN as string;
-
-
-  const client = twilio(accountSid, authToken);
-
-//console.log(id," ", currentUrl)
+   const client = twilio(accountSid, authToken);
+   //console.log(id," ", currentUrl)
+   
   try {
-    // const message = await client.messages.create({
-    //   body: `StudyPoint Boston invites you to apply for educational financing through Afford Financial. Use this secure link to apply: ${currentUrl}add?id=${id} `,
-    //   from: '+18449191857',
-    //   to: '+18777804236',
-    // });
+    const message = await client.messages.create({
+      body: `StudyPoint Boston invites you to apply for educational financing through Afford Financial. Use this secure link to apply: ${currentUrl}add?id=${id} `,
+      from: '+18449191857',
+      to: '+18777804236',
+    });
 
-    // return NextResponse.json({ body: message.body });
+     return NextResponse.json({ body: message.body });
   } catch (error) {
     return NextResponse.error();
   }
